a4109366d52b551bc47440068527b1a3f8dfaad8
valentina262004 <valengebze25@gmail.com>
2024-12-16 01:28:53 +0000

    4

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Hitung keliling dan luas")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+panjang = st.number_input("Masukan panjang persegi", value=0)
+st.write("The current number is ", panjang)
+
+lebar = st.number_input("Masukan lebar persegi", value=0)
+st.write("The current number is ", lebar)
+
+if (panjang & lebar):
+    st.subheader('Keliling')
+    st.write(2 * (panjang + lebar))
+    st.subheader('Luas')
+    st.write( panjang * lebar)
